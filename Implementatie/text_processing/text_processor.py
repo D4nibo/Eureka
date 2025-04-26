59e1ccdaa57255a78a4cfb9607d00a71e6f3c3fc
Boustani Daniel <boustanidaniel@hotmail.com>
2025-04-26 14:23:55 +0200

    Verwijderen van tijdelijke bestanden aangemaakt door Extractor/Chunker

--- Implementatie/text_processing/text_processor.py
+++ Implementatie/text_processing/text_processor.py
@@ -1,6 +1,7 @@
 from .markdown_extractor import MarkdownExtractor
 from .markdown_chunker import MarkdownChunker
-      
+import os
+
 class TextProcessor:
     __extraction_strategy = None
     __chunking_strategy = None
@@ -9,7 +10,15 @@ class TextProcessor:
         self.__extraction_strategy = extraction_strategy
         self.__chunking_strategy = chunking_strategy
 
+
+    def __empty_temp_dir(self):
+        for e in os.scandir('./text_processing/temp'):
+            if e.is_file():
+                os.remove(e.path)
+
+
     def extract_from(self, path):
         path_to_temp = self.__extraction_strategy.extract(path)
         chunks = self.__chunking_strategy.chunk(path_to_temp)
+        self.__empty_temp_dir() # Empty any temporary files that an Extractor and a Chunker could have made
         return chunks 
