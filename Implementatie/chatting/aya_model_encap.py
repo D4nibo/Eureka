2926dc95f1791421e6f1ea592124552546b00962
Boustani Daniel <boustanidaniel@hotmail.com>
2025-05-10 01:34:03 +0200

    OpenAI LLm toegevoegd (gpt-4.1.-mini-2025-04-14). Ondersteunong voor Engels en Nederlands

--- Implementatie/chatting/aya_model_encap.py
+++ Implementatie/chatting/aya_model_encap.py
@@ -1,7 +1,7 @@
 from langchain_community.llms import GPT4All
 from huggingface_hub import hf_hub_download
 
-class AyaModelEncap():
+class AyaModel():
     __huggingface_repo_id = 'QuantFactory/aya-23-8B-GGUF'
     __hugging_face_filename = 'aya-23-8B.Q4_0.gguf'
     __chat_model = None
@@ -10,7 +10,6 @@ class AyaModelEncap():
         self.__load_chat_model()
 
     def generate_answer(self, question, ctx=''):
-        
         system_directives = (
             '<|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>\n'
             'Jij bent Eureka, de vriendelijke virtuele assistent van HOGENT die studenten helpt bij het beantwoorden van vakgerelateerde vragen. '
@@ -27,7 +26,6 @@ class AyaModelEncap():
             f'Context:\n {ctx.strip()}\n'
             '<|END_OF_TURN_TOKEN|>'
         )
-
         
         prompt = (
             '<|BOS_TOKEN|>\n'
@@ -36,8 +34,6 @@ class AyaModelEncap():
             '<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>'
         )
 
-        print(prompt)
-        
         return self.__chat_model.invoke(prompt)
     
     def __load_chat_model(self):
---
+++ Implementatie/chatting/openai_model_encap.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+class OpenAIModel():
+    __model = None 
+    __model_name = 'gpt-4.1-mini-2025-04-14'
+
+    def __init__(self):
+        self.__init_model()
+
+    def generate_answer(self, question, ctx=''):
+        system_directive = (
+            '# Rol en Doelstelling\n'
+            'Je bent Eureka, de vriendelijke virtuele assistent van HOGENT. '
+            'Je helpt studenten door vragen te beantwoorden op basis van informatie die je intern tot je beschikking hebt.\n\n'
+
+            '# Instructies\n'
+            '- Beantwoord Engelstalige vragen in het Engels en Nederlandstalige vragen in het Nederlands.\n'
+            '- Gebruik uitsluitend de interne informatie om te antwoorden.\n'
+            '- Als de informatie voldoende is:\n'
+            '  - Formuleer een beknopt antwoord, vriendelijk en helder.\n'
+            '- Als er onvoldoende informatie beschikbaar is:\n'
+            '  - Stel op een vriendelijke manier voor om de vraag anders te formuleren of contact op te nemen met een docent.\n'
+            '- Noem nooit dat je informatie hebt ontvangen.\n'
+            '- Zeg nooit letterlijk 'de context', 'de informatie' of 'wat werd meegegeven'.\n\n'
+
+            '# Voorbeelden\'"
+            'Voorbeeld 1:\n'
+            'Vraag: Wat is de deadline van het project?\n'
+            'Context: Op 14 mei moet het project opgeladen worden op Chamilo\n'
+            'Antwoord: De deadline van het project is 14 mei. Zie ook de bijhorende handleiding.\n\n'
+
+            'Voorbeeld 2:\n'
+            'Vraag: Wanneer was de Tweede Wereldoorlog?\n'
+            'Context: SQL stands for Structured Query Language\n'
+            'Antwoord: Sorry, ik kan je daar momenteel niet mee helpen. Probeer de vraag anders te formuleren en anders neem je best contact op met je docent.\n\n'
+        
+            'Voorbeeld 3:\n'
+            'Vraag: Are retake exams possible ?\n'
+            'Context: Retake exams are possible in August.\n'
+            'Antwoord: Yes, retake exams are possible in August\n\n'
+
+            'Voorbeeld 4:\n'
+            'Vraag: Is group work mandatory?\n'
+            'Context: Group work is optional but strongly encouraged.\n'
+            'Antwoord: Group work is not mandatory, but it is strongly recommended.\n\n'
+        )
+
+        user_message = (
+            '# Context\n'
+            f'{ctx.strip()}\n\n'
+            '# Vraag\n'
+            f'{question.strip()}\n\n'
+        )
+        
+        return self.__model.create(
+            model = self.__model_name,
+            temperature = 0.8,
+            store = True,
+            instructions = system_directive,
+            input = user_message
+        ).output_text
+
+    def __init_model(self):
+        load_dotenv() 
+        client = OpenAI()
+        self.__model = client.responses
